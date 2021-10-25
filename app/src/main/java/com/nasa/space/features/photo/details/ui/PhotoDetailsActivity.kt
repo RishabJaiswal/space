88c127d900e68aa9dc57b3f2501b7cb339534444
RishabJaiswal <shivam.jaiswal175@gmail.com>
2021-10-25 17:06:47 +0530

    Photo details
    - create blank activity for photo details
    - create ui for photo details fragment

---
+++ app/src/main/java/com/nasa/space/features/photo/details/ui/PhotoDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.nasa.space.features.photo.details.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nasa.space.R
+
+class PhotoDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_details)
+    }
+}
